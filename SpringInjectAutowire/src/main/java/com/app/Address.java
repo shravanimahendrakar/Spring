7c2ce2ba897af9b918542317289f1aa86d0dce0c
shravanimahendrakar <mahendrakarshravani@gmail.com>
2024-10-03 09:56:25 -0400

    Add files via upload

---
+++ SpringInjectAutowire/src/main/java/com/app/Address.java
@@ -0,0 +1,29 @@
+package com.app;
+
+public class Address {
+
+	private String City;
+	private String State;
+	
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCity() {
+		return City;
+	}
+
+	public void setCity(String city) {
+		City = city;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public void setState(String state) {
+		State = state;
+	}
+	
+	
+}
